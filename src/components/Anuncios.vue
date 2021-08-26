4c9a63b45b684bf6adb2270006a36417b9db01c0
unknown <jlmv84Qgmail.com>
2021-08-26 17:20:44 -0400

    ventanas modal para anuncios que muestra imagen y mensaje

--- src/components/Anuncios.vue
+++ src/components/Anuncios.vue
@@ -2,7 +2,6 @@
   <div class="bg-anuncios">
     <v-container class="py-16">
       <v-row>
-        
         <v-col
           xs="12"
           sm="6"
@@ -12,63 +11,92 @@
           v-for="(gatito, i) in gatosDB"
           :key="i"
         >
-          <v-card class="cards" height="100%" @click.stop="dialog = true">
-            <v-card-title
-              class="
-                subtitle-1
-                font-weight-bold
-                card-title
-                grey
-                darken-4
-                white--text
-                justify-center
-                d-flex
-                mb-3
-              "
-            >
-              {{ gatito.nombre }}</v-card-title
-            >
-            <div class="mx-auto mb-8">
-              <v-img
-                v-if="gatito.img"
-                height="300"
-                :src="gatito.img"
-                contain
-                class="mb-3 image__item"
-              ></v-img>
-              <v-img
-                v-else
-                height="300"
-                class="mb-3"
-                src="../assets/gathogar_logo.png"
-              ></v-img>
+          <v-dialog max-width="350" :retain-focus="false">
+            <template v-slot:activator="{ on, attrs }">
+              <v-card
+                class="cards"
+                height="100%"
+                @click="dialog = true"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-card-title
+                  class="
+                    subtitle-1
+                    font-weight-bold
+                    card-title
+                    grey
+                    darken-4
+                    white--text
+                    justify-center
+                    d-flex
+                    mb-3
+                  "
+                >
+                  {{ gatito.nombre }}</v-card-title
+                >
+                <div class="mx-auto mb-8">
+                  <v-img
+                    v-if="gatito.img"
+                    height="300"
+                    :src="gatito.img"
+                    contain
+                    class="mb-3 image__item"
+                  ></v-img>
+                  <v-img
+                    v-else
+                    height="300"
+                    class="mb-3"
+                    src="../assets/gathogar_logo.png"
+                  ></v-img>
+                  <v-divider></v-divider>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-list-item-title class="card-text"
+                        >Cantidad: {{ gatito.cantidad }}</v-list-item-title
+                      >
+                      <v-list-item-title class="card-text"
+                        >Region: {{ gatito.region }}</v-list-item-title
+                      >
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-card-actions>
+                    <div class="div">
+                      <v-btn
+                        class="btn"
+                        v-if="logueado"
+                        @click="contacto(gatito.telefono)"
+                        color="cyan darken-3"
+                        >Te gustaría adoptarme?</v-btn
+                      >
+                    </div>
+                  </v-card-actions>
+                </div>
+              </v-card>
+            </template>
+            <v-card height="100%">
+              <v-card-title
+                class="
+                  subtitle-1
+                  font-weight-bold
+                  card-title
+                  grey
+                  darken-4
+                  white--text
+                  justify-center
+                  d-flex
+                  mb-3
+                "
+              >
+                {{ gatito.nombre }}</v-card-title
+              >
+              <v-img height="400" contain :src="gatito.img"></v-img>
               <v-divider></v-divider>
-              <v-list-item>
-                <v-list-item-content>
-                  <v-list-item-title class="card-text"
-                    >Cantidad: {{ gatito.cantidad }}</v-list-item-title
-                  >
-                  <v-list-item-title class="card-text"
-                    >Region: {{ gatito.region }}</v-list-item-title
-                  >
-                </v-list-item-content>
-              </v-list-item>
-              <v-card-text class="pt-0 card-text">
+              <v-card-text class="pt-5 card-text font-weight-black">
                 {{ gatito.mensaje }}
               </v-card-text>
-              <v-card-actions>
-                <div class="div">
-                  <v-btn
-                    class="btn"
-                    v-if="logueado"
-                    @click="contacto(gatito.telefono)"
-                    color="cyan darken-3"
-                    >Te gustaría adoptarme?</v-btn
-                  >
-                </div>
-              </v-card-actions>
-            </div>
-          </v-card>
+            </v-card>
+          </v-dialog>
         </v-col>
       </v-row>
     </v-container>
